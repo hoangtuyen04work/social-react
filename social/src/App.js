ce24c593299468a677edcad16e6e964a4967ab6c
hoangtuyen04work <164497260+hoangtuyen04work@users.noreply.github.com>
2024-08-12 14:00:38 +0700

    init project

---
+++ social/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+const App = () => {
+  return ( 
+    <div className = "app-container" >
+    Hello world 
+    </div>
+  )
+}
+
+export default App;
